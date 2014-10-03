c30f320eb11d8d808673828f15ae6ad39f3ad472
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-10-02 23:09:12 -0700

    change up the vibrance algorithm
    This makes more sense to me.

--- src/VibranceNode.cpp
+++ src/VibranceNode.cpp
@@ -11,41 +11,43 @@
 using namespace ci;
 using namespace Cinder::Pipeline;
 
+// Vibrance.fs from the ISF file shipped with VDMX5: http://vdmx.vidvox.net/blog/chroma-mask-audio-analysis-visualizations-and-more
 // NB - unlike Adobe's Vibrance, this does not treat skintones any differently
-// https://github.com/v002/v002-Color-Controls/blob/master/v002.Vibrance.frag
-// https://github.com/terrasque/sweetfxui/blob/master/SweetFX/SweetFX/Shaders/Vibrance.h
 const std::string FragmentShaderVibrance = R"(
     #version 120
     uniform sampler2D image;
     uniform float amount;
     uniform float mixAmount;
 
-    // http://en.wikipedia.org/wiki/Luma_(video)
-    float lumaRec601(vec4 color) {
-        return dot(color.rgb, vec3(0.299, 0.587, 0.114));
-    }
-    // http://en.wikipedia.org/wiki/Luminance_(colorimetry)
-    float lumaRec709(vec4 color) {
-        return dot(color.rgb, vec3(0.2126, 0.7152, 0.0722));
+    // http://gamedev.stackexchange.com/a/59808
+    vec3 rgb2hsv(vec3 c) {
+        vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+        vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+        vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+        float d = q.x - min(q.w, q.y);
+        float e = 1.0e-10;
+        return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
     }
 
-    // https://www.opengl.org/sdk/docs/man/html/sign.xhtml
-    float sign(float value) {
-        return (value < 0.0) ? -1.0 : ((value == 0.0) ? 0.0 : 1.0);
+    vec3 hsv2rgb(vec3 c) {
+        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
     }
 
     void main() {
         vec2 position = gl_TexCoord[0].xy;
         vec4 color = texture2D(image, position);
 
-        float y = lumaRec709(color);
-        float max = max(color.r, max(color.g, color.b));
-        float min = min(color.r, min(color.g, color.b));
-        float delta = max - min; // max - min ~= saturation
+        vec3 c = rgb2hsv(color.rgb);
+        // plot:  http://po.st/lVilpL
+        c.y = amount * (sqrt(c.y) - c.y) + c.y;
+        c.y = clamp(c.y, 0.0, 1.0);
 
         vec4 result;
-        result.rgb = mix(vec3(y), color.rgb, clamp(1.0 + (amount * (1.0 - (sign(amount) * delta))), 0.0, 1.0));
-        result.a = 1.0;
+        result.rgb = hsv2rgb(c);
+        result.a = color.a;
 
         gl_FragColor = mix(color, result, mixAmount);
     }
@@ -54,7 +56,7 @@ const std::string FragmentShaderVibrance = R"(
 VibranceNode::VibranceNode() {
     std::vector<NodePortRef> inputPorts = {
         NodePort::create(NodeInputPortKeyImage, NodePortType::FBOImage),
-        NodePort::create(VibranceNodeInputPortKeyAmount, NodePortType::Float, "Amount", 0.0f, -1.0f, 1.0f),
+        NodePort::create(VibranceNodeInputPortKeyAmount, NodePortType::Float, "Amount", 0.0f, -0.6f, 0.6f),
         NodePort::create(VibranceNodeInputPortKeyMixAmount, NodePortType::Float, "Mix", 1.0f, 0.0f, 1.0f),
     };
     setInputPorts(inputPorts);
