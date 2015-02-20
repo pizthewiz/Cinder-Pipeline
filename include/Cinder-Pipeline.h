5735352298a35d01b32b8adf62b50860d4795086
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2015-02-19 22:32:31 -0800

    remove support whitelist
    Hopefully things work on Windows, though it is entirely untested.

--- include/Cinder-Pipeline.h
+++ include/Cinder-Pipeline.h
@@ -3,14 +3,10 @@
 //  Cinder-Pipeline
 //
 //  Created by Jean-Pierre Mouilleseaux on 19 Apr 2014.
-//  Copyright 2014 Chorded Constructions. All rights reserved.
+//  Copyright 2014-2015 Chorded Constructions. All rights reserved.
 //
 
 #pragma once
-
-#if !defined(CINDER_MAC)
-    #error Target platform unsupported by Cinder-Pipeline
-#endif
 
 #include "Node.h"
 #include "SourceNode.h"
